f34f9c050fd87c92ea3be5071d56232a6411166e
mrodavell <jomar.llevado@ustp.edu.ph>
2020-08-18 11:01:11 +0800

    Initial Deployment

---
+++ lgu/precache-manifest.e108b52e662fc3f5403caba7277f0cd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234e78dbb39e30828a6f5bbe98c2b9ff",
+    "url": "/marina/module/lgu/index.html"
+  },
+  {
+    "revision": "9c4e99b3cd0bcc54524c",
+    "url": "/marina/module/lgu/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9d74988a307c197754ab",
+    "url": "/marina/module/lgu/static/css/main.112126e9.chunk.css"
+  },
+  {
+    "revision": "9c4e99b3cd0bcc54524c",
+    "url": "/marina/module/lgu/static/js/2.f9f69580.chunk.js"
+  },
+  {
+    "revision": "7b1affef45e2b57a268c1974299a48a9",
+    "url": "/marina/module/lgu/static/js/2.f9f69580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d74988a307c197754ab",
+    "url": "/marina/module/lgu/static/js/main.52282aa9.chunk.js"
+  },
+  {
+    "revision": "ca0c44fa71c3ac10de0a",
+    "url": "/marina/module/lgu/static/js/runtime-main.c0833273.js"
+  },
+  {
+    "revision": "25bc756053a3e40ef43661e0f7a6e525",
+    "url": "/marina/module/lgu/static/media/app.25bc7560.apk"
+  },
+  {
+    "revision": "1f15fbb3dd83cc6df9c32558ed3a2bc5",
+    "url": "/marina/module/lgu/static/media/dto.1f15fbb3.png"
+  },
+  {
+    "revision": "3d4d22a69af0e44ffed02d4beb5cb57a",
+    "url": "/marina/module/lgu/static/media/logo.3d4d22a6.png"
+  },
+  {
+    "revision": "066df2abafbd8c2b31ba704f9798c9d7",
+    "url": "/marina/module/lgu/static/media/user-manual.066df2ab.pdf"
+  }
+]);
